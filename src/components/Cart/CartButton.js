d858c6ae8798c252eca12cb3bc9369c25fd538ad
Wojciech Berezowski <61703808+wkberezowski@users.noreply.github.com>
2022-05-10 13:22:33 +0200

    added redux for cart

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,10 +1,18 @@
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './CartButton.module.css';
+import { uiActions } from '../../store/ui-slide';
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const cartQuantity = useSelector((state) => state.cart.totalQuantity);
+
+  const toggleCartHanlder = () => {
+    dispatch(uiActions.toggle());
+  };
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCartHanlder}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
--- src/store/ui-slide.js
+++ src/store/ui-slide.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-createSlice({
+const uiSlice = createSlice({
   name: 'ui',
   initialState: { cartIsVisible: false },
   reducers: {
@@ -9,3 +9,7 @@ createSlice({
     },
   },
 });
+
+export const uiActions = uiSlice.actions;
+
+export default uiSlice;
